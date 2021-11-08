9d161b53d788432933eee66de51996d1f4af8fc1
cocofox1902 <colas.renard1902@gmail.com>
2021-11-08 09:50:47 +0000

    add library

--- main.h
+++ main.h
@@ -1,6 +1,19 @@
 #ifndef PRINTF
 #define PRINTF
 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+typedef struct specifier
+{
+	char *valid;
+	int (*f)(va_list);
+} spec;
 int _printf(const char *format, ...);
+int _putchar(char c);
+int (*_flag(char j))(va_list);
+int int_print_d(va_list f);
+int int_print_i(va_list f);
 
 #endif /* PRINTF */
