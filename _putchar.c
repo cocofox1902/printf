a897974bfa1989cca3162925ca891ab170c23e3c
cocofox1902 <colas.renard1902@gmail.com>
2021-11-09 14:11:39 +0000

    add betty style

--- _putchar.c
+++ _putchar.c
@@ -12,3 +12,17 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ * Return: number of chars written
+ */
+int _puts(char *str)
+{
+	register int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+		_putchar(str[x]);
+	return (x);
+}
