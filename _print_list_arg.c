9efdffbaaf5eb8f5f148a7c4ad27efe2b552b6ab
cocofox1902 <colas.renard1902@gmail.com>
2021-11-10 09:02:30 +0000

    other way for 4

--- _print_list_arg.c
+++ _print_list_arg.c
@@ -1,59 +1,7 @@
 #include "main.h"
 
-int int_print_u(va_list f);
-int int_print_o(va_list f);
-int int_print_x(va_list f);
-int int_print_X(va_list f);
 int int_print_r(va_list f);
 int int_print_p(va_list f);
-
-/**
- * int_print_u - print
- * @f: f
- * Return: f
- */
-
-int int_print_u(va_list f)
-{
-	int_print_u_arg(va_arg(f, unsigned int));
-	return (0);
-}
-
-/**
- * int_print_o - print
- * @f: f
- * Return: f
- */
-
-int int_print_o(va_list f)
-{
-	int_print_o_arg(va_arg(f, unsigned int));
-	return (0);
-}
-
-/**
- * int_print_x - print
- * @f: f
- * Return: f
- */
-
-int int_print_x(va_list f)
-{
-	int_print_x_arg(va_arg(f, unsigned int));
-	return (0);
-}
-
-/**
- * int_print_X - print
- * @f: f
- * Return: f
- */
-
-int int_print_X(va_list f)
-{
-	int_print_X_arg(va_arg(f, unsigned int));
-	return (0);
-}
 
 /**
  * int_print_r - print
